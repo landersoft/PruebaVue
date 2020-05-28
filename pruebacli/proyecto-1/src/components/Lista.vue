bab5fbc585574811112a7f61a20712dfe7164e30
Rodrigo Eduardo Lander Sepulveda <Rodrigo.Lander@BUPA.CL>
2020-05-27 21:16:13 -0400

    se agregan componentes y mutaciones.

---
+++ pruebacli/proyecto-1/src/components/Lista.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul class="list-group">
+      <li
+        v-for="(item, index) of frutas"
+        :key="item.id"
+        @click="aumentar(index)"
+        class="list-group-item d-flex justify-content-between align-items-center"
+      >
+        {{index}} - {{item.nombre}}
+        <span class="badge badge-primary badge-pill">{{item.cantidad}}</span>
+      </li>
+    </ul>
+    <span>
+      <button class="btn btn-danger" @click="reiniciar">Reiniciar</button>
+    </span>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  name: "Lista",
+  computed: {
+    ...mapState(["frutas"]),
+    arrayOrdena() {}
+  },
+  methods: {
+    ...mapMutations(["aumentar", "reiniciar"])
+  }
+};
+</script>
